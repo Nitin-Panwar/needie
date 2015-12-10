49e5beeef44832f9042910508584c78aceab1163
Roopak K Prajapat <roopak.k.prajapat@intel.com>
2015-12-10 18:30:14 +0530

    metric data table layout

--- client/app/metrics/modals/modal.controller.js
+++ client/app/metrics/modals/modal.controller.js
@@ -47,6 +47,70 @@ angular.module('sasaWebApp')
       $scope.cancel = function(){
         $modalInstance.dismiss('Canceled');
       }; // end done
+
+
+      $scope.availableColoumns = {
+        items: ['column1','column2','column3','column4','column5','column6'],
+        selected: []
+      };
+      $scope.selectedColumns = {
+        items:[],
+        selected: []
+      };
+
+      /**
+       * this function selets items to add to data grid
+       * @param  {[type]} item    [description]
+       * @param  {[type]} boolean [description]
+       * @return {[type]}         [description]
+       */
+      $scope.dataGrid = function (item, boolean) {
+        if(boolean){
+          // select column to show in data grid
+          for(var i in $scope.availableColoumns.selected) {
+            $scope.selectedColumns.items.push($scope.availableColoumns.selected[i]);
+            $scope.availableColoumns.items.splice($scope.availableColoumns.items.indexOf($scope.availableColoumns.selected[i]), 1);            
+          }
+          $scope.availableColoumns.selected.length = 0;
+        }
+        else
+        {
+          // remove from selection
+          for(var i in $scope.selectedColumns.selected){
+            $scope.availableColoumns.items.push($scope.selectedColumns.selected[i]);
+            $scope.selectedColumns.items.splice($scope.selectedColumns.items.indexOf($scope.selectedColumns.selected[i]), 1);    
+          }
+          $scope.selectedColumns.selected.length = 0;
+        }
+      }
+
+      /**
+       * this function selects columns to show in data grid
+       * @param  {[type]} item    [description]
+       * @param  {[type]} boolean [description]
+       * @return {[type]}         [description]
+       */
+      $scope.selection = function (item, boolean) {
+        if(boolean){          
+          // select column to show in data grid          
+          if($scope.availableColoumns.selected.indexOf(item) === -1){
+            $scope.availableColoumns.selected.push(item);
+          }
+          else{
+            $scope.availableColoumns.selected.splice($scope.availableColoumns.selected.indexOf(item), 1);  
+          }
+        }
+        else
+        {
+          // remove from selection
+          if($scope.selectedColumns.selected.indexOf(item) === -1){
+            $scope.selectedColumns.selected.push(item);
+          }
+          else{
+            $scope.selectedColumns.selected.splice($scope.selectedColumns.selected.indexOf(item), 1);  
+          }
+        }
+      }
       
     })
 
