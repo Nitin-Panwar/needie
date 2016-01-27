67fa846421a93b696ec451f95efb73e9943ea115
Panwar, Nitin <nitin.panwar@intel.com>
2016-01-27 16:04:20 +0530

    Minor fixes

--- client/app/metrics/metric-card/metric-card.directive.js
+++ client/app/metrics/metric-card/metric-card.directive.js
@@ -82,6 +82,8 @@ angular.module('sasaWebApp')
         //Variable to change vizualization
         scope.line = false;
 
+        scope.items = [1,2,3,4,5,6,7,8,9,10]
+
         /**
          * [function to change vizualization]
          * @param  {[type]} type [description]
