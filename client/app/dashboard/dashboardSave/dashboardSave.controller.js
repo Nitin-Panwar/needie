63655f9f540be4c069f5c439323070b980779d80
Panwar, Nitin <nitin.panwar@intel.com>
2016-03-22 16:05:16 +0530

    Resolved git issues

--- client/app/dashboard/dashboardSave/dashboardSave.controller.js
+++ client/app/dashboard/dashboardSave/dashboardSave.controller.js
@@ -7,82 +7,50 @@ angular.module('sasaWebApp')
     $scope.OldDescription = $rootScope.placeholder.dashboard.description; 
     $scope.exist = false;
 
-    /**
-       * to apply the dialog
-       * @param  {[type]} which [description]
-       * @return {[type]}       [description]
-       */
-      $scope.save = function(flag){
-        //for creating new dashboard 
-        if(flag===true){
-          $scope.save_dashboard = dashBoardsFactory.index().$promise.then(function(dashboards){            
-            $scope.dashboardList = dashboards;
-            for(var i=0;i<$scope.dashboardList.length;i++){
-              if(data.name===$scope.dashboardList[i].name){
-                messageCenterService.add('danger','Dashboard exists with same name.Please try different name.',{timeout: 10000});
-                $scope.exist = true;
-                $scope.dashboardUrl = $scope.dashboardList[i]['_id']['$oid']
-                return;
-              }
+    //Save dialogue
+    $scope.save = function(flag){
+      //for creating new dashboard 
+      if(flag===true){
+        $scope.save_dashboard = dashBoardsFactory.index().$promise.then(function(dashboards){ 
+          //Getting dashboard list and assign it to other variable            
+          $scope.dashboardList = dashboards;
+          for(var i=0;i<$scope.dashboardList.length;i++){
+            if(data.name===$scope.dashboardList[i].name){
+              messageCenterService.add('danger','Dashboard exists with same name.Please try different name.',{timeout: 10000});
+              $scope.exist = true;
+              $scope.dashboardUrl = $scope.dashboardList[i]['_id']['$oid']
+              return;
             }
-            // Remove dashboard id for creating new dashboard
-            delete $rootScope.placeholder.dashboard._id;
-            $modalInstance.close(data);
-          });
-        }
-
-        //For overwriting existing dashboard
-        else{
-          $scope.save_dashboard = dashBoardsFactory.index().$promise.then(function(dashboards){            
-            $scope.dashboardList = dashboards;
-            for(var i=0;i<$scope.dashboardList.length;i++){
-              if(data.name===$scope.dashboardList[i].name){
-                if($scope.OldName === data.name && $rootScope.placeholder.dashboard.owner === $rootScope.user){continue;}
-                messageCenterService.add('danger','Dashboard exists with same name.Please try different name.',{timeout: 10000});
-                $scope.exist = true;
-                $scope.dashboardUrl = $scope.dashboardList[i]['_id']['$oid']
-                return;
-              }
+          }
+          // Remove dashboard id for creating new dashboard
+          delete $rootScope.placeholder.dashboard._id;
+          $modalInstance.close(data);
+        });
+      }
+      //For overwriting existing dashboard
+      else{
+        $scope.save_dashboard = dashBoardsFactory.index().$promise.then(function(dashboards){            
+          $scope.dashboardList = dashboards;
+          for(var i=0;i<$scope.dashboardList.length;i++){
+            if(data.name===$scope.dashboardList[i].name){
+              if($scope.OldName === data.name && $rootScope.placeholder.dashboard.owner === $rootScope.user){continue;}
+              messageCenterService.add('danger','Dashboard exists with same name.Please try different name.',{timeout: 10000});
+              $scope.exist = true;
+              $scope.dashboardUrl = $scope.dashboardList[i]['_id']['$oid']
+              return;
             }
-            $modalInstance.close(data);
-          });
-        }
-        // in case its a new dashboard, create a new object for dashboard
-        if($scope.OldName !== data.name){
-          
-        }
-
-        // validate for existing dashboards with same name if current user is not dashboard owner
-        // if($rootScope.placeholder.dashboard.owner !== $rootScope.user){          
-        //   $scope.save_dashboard =  dashBoardsFactory.index().$promise.then(function(dashboards){            
-        //     $scope.dashboardList = dashboards;
-        //     for(var i=0;i<$scope.dashboardList.length;i++){
-        //       if(data.name===$scope.dashboardList[i].name){
-        //         messageCenterService.add('danger','Dashboard exists with same name.Please try different name.',{timeout: 10000});
-        //         $scope.exist = true;
-        //         $scope.dashboardUrl = $scope.dashboardList[i]['_id']['$oid']
-        //         return;
-        //       }
-        //     }
-        //     $modalInstance.close(data);
-        //   });
-        // }
-        // else{
-        //   $modalInstance.close(data);
-        // }
-              
-      };
+          }
+          $modalInstance.close(data);
+        });
+      }
+      // in case its a new dashboard, create a new object for dashboard
+      if($scope.OldName !== data.name){}      
+    };
       
-      /**
-       * to close the dialog
-       * @return {[type]} [description]
-       */
-      $scope.cancel = function(){
-
-        $rootScope.placeholder.dashboard["name"] = $scope.OldName;
-        $rootScope.placeholder.dashboard["description"] =  $scope.OldDescription;
-        $modalInstance.dismiss('Canceled');
-      
+    //To close the dialog
+    $scope.cancel = function(){
+      $rootScope.placeholder.dashboard["name"] = $scope.OldName;
+      $rootScope.placeholder.dashboard["description"] =  $scope.OldDescription;
+      $modalInstance.dismiss('Canceled');
     }; 
-
   });
--- client/app/metrics/modals/modal.controller.js
+++ client/app/metrics/modals/modal.controller.js
@@ -1,7 +1,6 @@
 'use strict';
 
 angular.module('sasaWebApp')
-
 .controller('ModalCtrl',function($scope,$modalInstance,data,$rootScope,metricsFactory, messageCenterService){
       $scope.showColumns = true;
       $scope.showApplyButton = false;
@@ -26,24 +25,7 @@ angular.module('sasaWebApp')
       $scope.filterSubData = {};
       $scope.disableApplyButton =[];
 
-      ///////////////////////////////////////////////////
-      $scope.color = {
-        red: Math.floor(Math.random() * 255),
-        green: Math.floor(Math.random() * 255),
-        blue: Math.floor(Math.random() * 255)
-      };
-      $scope.rating1 = 3;
-      $scope.rating2 = 2;
-      $scope.rating3 = 4;
-      $scope.disabled1 = 0;
-      $scope.disabled2 = 70;
-      ///////////////////////////////////////////////////
-
-      /**
-       * toggles active state
-       * @param  {[type]} argument [description]
-       * @return {[type]}          [description]
-       */
+      //Toggles active state
       $scope.toggelActive = function (argument) {        
         if($scope.measureInfo[argument].active == undefined){
           $scope.measureInfo[argument].active = false;
@@ -53,29 +35,23 @@ angular.module('sasaWebApp')
         }        
       }
 
-      /**
-      * this function populates all metric columns
-       * @param  {[type]} argument [description]
-       * @return {[type]}          [description]
-       */
+      //This function populates all metric columns
       $scope.getMetricColumns = function (argument) {
-
-        if(!$scope.data.gridColumns){$scope.data.gridColumns = [];}
+        if(!$scope.data.gridColumns){
+          $scope.data.gridColumns = [];
+        }
         if($scope.data.gridColumns.length !== 0){          
           $scope.selectedColumns.items = $scope.data.gridColumns;    
         }
         if($scope.availableColoumns.items.length !== 0){
           return;
         }       
-
         $rootScope.metricPromise = metricsFactory.getColumns({dataset: $scope.data.dataset}).$promise.then(function (response) {                    
           var columns = response;
           for(var i in $scope.data.gridColumns){
               columns.splice(columns.indexOf($scope.data.gridColumns[i]), 1);
           }
-
           $scope.availableColoumns.items = columns;          
-
           if($scope.data.gridColumns){
             $scope.selectedColumns.items = $scope.data.gridColumns;
           }
@@ -84,10 +60,7 @@ angular.module('sasaWebApp')
         })
       };
 
-      /**
-       * these are options for data grid
-       * @type {Object}
-       */
+      //These are options for data grid
       $scope.gridOptions = {
         enableSorting: true,
         enableColumnResizing: true,
@@ -115,31 +88,25 @@ angular.module('sasaWebApp')
         columnDefs: [],
         data: []
       };
-                /**
-       * this function gets raw metric data for selected columns
-       * @return {[type]} [description]
-       */
+
+      //This function gets raw metric data for selected columns
       $scope.getRawData = function (offset) {
         $scope.offset = offset;
         $scope.csvData.data = undefined;  
         $scope.gridOptions.data = [];      
         $scope.gridOptions.columnDefs = [];
         $scope.showColumns = false;
-
         if($scope.selectedColumns.items.length === 0){
           messageCenterService.add('danger','Please select columns', {timeout: 10000});
           return;
         }
-
         var filters = angular.extend({}, $rootScope.globalQuery, data.filters);
-
         $rootScope.metricPromise = metricsFactory.getRawData({fields: $scope.selectedColumns.items, metricId: $scope.data._id, filters: filters, offset: $scope.offset}).$promise.then(function (response) {          
           if(offset === 'all'){
             $scope.csvData.data = response;
             $scope.csvData.headers = Object.keys(response[0]);
             return;
           }
-
           $scope.gridOptions.data = response;       
           // create column definitions
           for (var column in $scope.selectedColumns.items){
@@ -155,11 +122,7 @@ angular.module('sasaWebApp')
       };
                                 
 
-      /**
-       * to apply the dialog
-       * @param  {[type]} which [description]
-       * @return {[type]}       [description]
-       */
+      //To apply the dialog
       $scope.save = function(which){
         $rootScope.placeholder.edited = true;
         switch(which){
@@ -169,21 +132,6 @@ angular.module('sasaWebApp')
           case 'measure':
             for( var i in $scope.measureInfo){
               $scope.measureInfo[i].goal=$scope.goal[i];
-              //Not using threshold as of now
-              // $scope.measureInfo[i].threshold ={};
-              // for(var key in $scope.measureInfo[i]){
-              // if(key=='threshold' && $scope.tempThreshold[i]) {
-              //   $scope.measureInfo[i].threshold.upperWarning = $scope.tempThreshold[i].uw;
-              //   $scope.measureInfo[i].threshold.upperAlert = $scope.tempThreshold[i].ua;
-              //   $scope.measureInfo[i].threshold.lowerWarning = $scope.tempThreshold[i].lw;
-              //   $scope.measureInfo[i].threshold.lowerAlert = $scope.tempThreshold[i].la;  
-              //   console.log($scope.goal)
-              //   $scope.measureInfo[i].goal = $scope.goal[i];
-              // }
-              // if(key=='goal'){
-              //   $scope.measureInfo[i].goal = $scope.goal[i];
-              //   console.log($scope.$scope.measureInfo[i])
-              // }
             }
             delete $scope.tempThreshold;
             delete $scope.goal;
@@ -199,15 +147,12 @@ angular.module('sasaWebApp')
         }
       };
       
-      /**
-       * to close the dialog
-       * @return {[type]} [description]
-       */
+      // To close the dialog
       $scope.cancel = function(){
         $modalInstance.dismiss('Canceled');
-      }; // end done
+      }; // end cancel
 
-
+      //To compare threshold vlaues
       $scope.compareThreshold = function(){
         for( var i in $scope.tempThreshold){
           if(($scope.tempThreshold[i].ua < $scope.tempThreshold[i].uw) || ($scope.tempThreshold[i].la > $scope.tempThreshold[i].lw))
@@ -215,12 +160,7 @@ angular.module('sasaWebApp')
         } 
       }
 
-      /**
-       * this function selets items to add to data grid
-       * @param  {[type]} item    [description]
-       * @param  {[type]} boolean [description]
-       * @return {[type]}         [description]
-       */
+      //This function selets items to add to data grid
       $scope.dataGrid = function (item, boolean) {
         if(boolean){
           // select column to show in data grid
@@ -241,12 +181,7 @@ angular.module('sasaWebApp')
         }
       };
 
-      /**
-       * this function selects columns to show in data grid
-       * @param  {[type]} item    [description]
-      * @param  {[type]} boolean [description]
-       * @return {[type]}         [description]
-       */
+      //This function selects columns to show in data grid
       $scope.selection = function (item, boolean) {
         if(boolean){          
           // select column to show in data grid          
@@ -293,12 +228,7 @@ angular.module('sasaWebApp')
         }
       }
 
-      /**
-       * find unique items in an array by key      
-       * @param  {[type]} array [description]
-       * @param  {[type]} key   [description]
-       * @return {[type]}       [description]
-       */
+      //Find unique items in an array by key      
       $scope.pluck = function(arr, key, matchKey, value) {
         if(value && matchKey){
           var result = $.map(arr, function(e) { 
