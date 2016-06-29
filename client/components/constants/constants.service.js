d3596a4e1d13f4bb9d1e36f895e834a048ccc728
Karishma <karishma.chug@intel.com>
2016-06-29 16:25:29 +0530

    UI bug fixes

--- client/components/constants/constants.service.js
+++ client/components/constants/constants.service.js
@@ -8,8 +8,6 @@ angular.module('sasaWebApp')
   .constant('webServiceURL', {
   	// web service server url
 	'url': 'http://vmssasadevapp1.gar.corp.intel.com:5050',	
-
-	// 'url': 'http://npanwar-mobl.gar.corp.intel.com:5050',	
 	'config': { 'Content-Type': 'application/json; } charset=UTF-8' },
 	'loginUrl': 'http://vmsenggds.gar.corp.intel.com/getUserDetails'
 });
