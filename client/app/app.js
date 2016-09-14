ac86a42d75b8a850199eb9467ff105229ebac078
Jena <pratapx.jena@intel.com>
2016-09-14 10:10:47 +0530

    minor changes

--- client/app/app.js
+++ client/app/app.js
@@ -64,12 +64,18 @@ angular.module('sasaWebApp', [
 
 .run(
   function ($rootScope, $http, webServiceURL, messageCenterService,dashBoardsFactory,$location, usersFactory,$stateParams) {
+         if(webServiceURL.url === "http://sminsights-api.intel.com"){
+                 var dynamicScript = document.createElement('script'); 
+                  dynamicScript.src = 'http://appusage.intel.com/Service/api/loguser/13806';
+                  dynamicScript.async = 'false';
+                  document.body.appendChild(dynamicScript);
+          }
     //Login user if not logged in
     if($rootScope.user == undefined){ 
       $rootScope.myPromise = $http.get(webServiceURL.loginUrl,{withCredentials:true}).then(function (response) {     
         $rootScope.userDetails = response.data.user;
         $rootScope.user = $rootScope.userDetails['idsid'].toLowerCase();
-       //  $rootScope.user = 'gar\\pjenax'
+        // $rootScope.user = 'gar\\pjenax'
         //find user homepage    
         $rootScope.myPromise= usersFactory.get({user:$rootScope.user}).$promise.then(function (data) {
              // console.log("user info",data)
