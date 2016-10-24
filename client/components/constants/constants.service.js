583ac48488fd5b1c561b5bafcc2cdcfa643d4ce3
Jena <pratapx.jena@intel.com>
2016-10-24 16:47:36 +0530

    Ui defects and score card api call issue

--- client/components/constants/constants.service.js
+++ client/components/constants/constants.service.js
@@ -7,7 +7,7 @@ angular.module('sasaWebApp')
 */
   .constant('webServiceURL', {
   	// web service server url
-	'url': 'http://ikhurana-mobl.gar.corp.intel.com:5050',	
+	'url': 'http://vmssmmconsapp1.amr.corp.intel.com:5000',	
 	'config': { 'Content-Type': 'application/json; } charset=UTF-8' },
 	'loginUrl': 'http://vmsenggds.gar.corp.intel.com/getUserDetails'
 });
